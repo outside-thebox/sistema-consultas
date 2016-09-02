64c6e846d15365e780e33a779e5e724439aafa9d
vagrant <vagrant@homestead>
2016-09-02 02:30:56 +0000

    Agrego cambios para agregar criterios de busqueda

--- resources/views/busquedas/functions_scripts.blade.php
+++ resources/views/busquedas/functions_scripts.blade.php
@@ -6,6 +6,14 @@
         $("select[name='donde']").append("<option value=''>Seleccione</option>");
     }
 
+
+    function limpiarCampos()
+    {
+        $("select[name='donde']").val("");
+        $("input:text[name='texto_a_buscar']").val("");
+    }
+
+
     function obtenerCamposHechos(selected)
     {
         selected = selected || "";
@@ -15,8 +23,8 @@
 
         var url = "{{route('api.v1.hecho.getFields')}}";
         $.getJSON(url,function(data){
-            $.each(data, function(k,v){
-                $("select[name='donde']").append("<option value=\""+ k+"\">"+ v+"</option>");
+            $.each(data, function(id,nombre_campo){
+                $("select[name='donde']").append("<option value=\""+ nombre_campo+"\">"+ nombre_campo +"</option>");
             });
         });
     }
@@ -27,11 +35,10 @@
         $("select[name='donde']").empty();
         $("select[name='donde']").append("<option value=''>Seleccione</option>");
 
-
         var url = "{{route('api.v1.personas.getFields')}}";
         $.getJSON(url,function(data){
-            $.each(data, function(k,v){
-                $("select[name='donde']").append("<option value=\""+ k+"\">"+ v+"</option>");
+            $.each(data, function(id,nombre_campo){
+                $("select[name='donde']").append("<option value=\""+ nombre_campo +"\">"+ nombre_campo +"</option>");
             });
         });
     }
--- resources/views/busquedas/index.blade.php
+++ resources/views/busquedas/index.blade.php
@@ -29,14 +29,21 @@
                     limpiar();
             });
 
+            $("#btnAgregarCriterio").click(function(){
+                var donde = $("select[name='donde']").val();
+                var es = $("select[name='es']").val();
+                var texto_a_buscar = $("input:text[name='texto_a_buscar']").val();
 
+                if(donde != "" && texto_a_buscar != "")
+                {
+                    $("#criterios_utilizados").append("<li>" + donde + " = " + texto_a_buscar + "</li>");
+                    limpiarCampos();
+                }
+                else
+                    alert("Debe completar el campo 'donde' y tambien el texto");
 
-
+            });
         });
-
-
-
-
     </script>
 
 
@@ -52,12 +59,22 @@
             </div>
             <div class="form-group col-md-6">
                 {{ Form::label('donde', 'Donde',[]) }}
-                {{ Form::select('donde',['' => 'Seleccione el campo','campo1' => 'campo1','campo2' => 'campo2','campo3' => 'campo3'], null, array('id' => 'id_tipo_reconocimiento', 'class' => 'form-control')) }}
+                {{ Form::select('donde',['' => 'Seleccione'], null, ['class' => 'form-control']) }}
             </div>
             <div class="form-group col-md-6">
                 {{ Form::label('es', 'Es',[]) }}
-                {{ Form::select('es',['igual_a' => 'Igual a','contiene' => 'Contiene (Texto)'], null, array('id' => 'id_tipo_reconocimiento', 'class' => 'form-control')) }}
+                {{ Form::select('es',['=' => 'Igual a','like' => 'Contiene (Texto)'], null, array('id' => 'id_tipo_reconocimiento', 'class' => 'form-control')) }}
+            </div>
+            <div class="col-md-6 form-inline">
+                {{ Form::text( 'texto_a_buscar',null, ['class' => 'form-control','maxlength' => 20, 'style'=>'margin-top: 25px']) }}
+                <button type="button" class="btn btn-success btn-sm" id="btnAgregarCriterio" style="margin-top: 25px">Agregar criterio</button>
             </div>
         </div>
+        <strong>Criterios utilizados</strong>
+        <div class="row" id="criterios_utilizados">
+
+        </div>
+
+
     </div>
 @endsection
